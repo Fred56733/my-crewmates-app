e8ee2c47bda033fcc5e1dcc0857cc19930e651e6
Fred56733 <fred.debiase55@gmail.com>
2025-04-11 13:24:35 -0400

    added drop down for colors and styling

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -10,7 +10,18 @@ const Create = () => {
                 <input type="text" id="name" name="name" required />
 
                 <label htmlFor="color">Color:</label>
-                <input type="text" id="color" name="color" required />
+                <select id="color" name="color" required>
+                    <option value="">Select a color</option>
+                    <option value="black">Black</option>
+                    <option value="cyan">Cyan</option>
+                    <option value="green">Green</option>
+                    <option value="orange">Orange</option>
+                    <option value="pink">Pink</option>
+                    <option value="purple">Purple</option>
+                    <option value="red">Red</option>
+                    <option value="tan">Tan</option>
+                    <option value="yellow">Yellow</option>
+                </select>
 
                 <label htmlFor="description">Description:</label>
                 <textarea id="description" name="description" required></textarea>
@@ -19,6 +30,6 @@ const Create = () => {
             </form>
         </div>
     );
-}
+};
 
 export default Create;
