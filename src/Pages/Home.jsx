d951687d894b737ead78df8c254235ad8b928f04
Fred56733 <fred.debiase55@gmail.com>
2025-04-11 14:09:55 -0400

    added img of all crew to homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className="home">
         <h1>Welcome to the Crewmate Creator</h1>
+        <img src="/src/assets/all crew.png" alt="Crewmate" className="home-image" />
         <p>Create and Explore a collection of your crewmates before they get shipped off into space!</p>
         </div>
     );
